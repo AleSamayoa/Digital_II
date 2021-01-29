6c1aa4972e2f873c728a526caf2ff5c31d44c149
AleSamayoa <69053381+AleSamayoa@users.noreply.github.com>
2021-01-29 09:34:40 -0600

    Pseudo y Prelab correcto

--- Lab2/Lab2.X/lab_2.c
+++ Lab2/Lab2.X/lab_2.c
@@ -33,7 +33,8 @@
 // Variables
 //******************************************************************************
 
-char counter = 0; //para el contador 
+uint8_t contador; //para el contador 
+//Variable que incrementa con el boton
 
 
 //******************************************************************************
@@ -55,6 +56,7 @@ void main(void) {
     //**************************************************************************
 
     while (1) {
+        //switch case? para encender los 7 segmentos
 
        
              
@@ -67,13 +69,18 @@ void main(void) {
 //******************************************************************************
 
 void setup(void) {
+    //configurar para interrupciones
     di();
     ei();
+    //ver configuracion para adc
+    PIE1bits.ADIE=1;
+    PIR1bits.ADIF=0;
+    
     TRISE = 0;
     PORTE = 0;
     ANSEL = 0;
     ANSELH = 0;
-    TRISB = 0b00000111;
+    TRISB = 0b00000011;
     PORTB = 0;
     TRISC = 0;
     PORTC = 0;
@@ -84,12 +91,25 @@ void setup(void) {
 // Funciones
 //******************************************************************************
 void main (){
+    void inc(){
+        if (PORTBbits.RB0==1)
+        contador ++;
+    }
+    //buscar funcion que lo resta pero hacer lo mismo con rb1
+    void rest(){
+        if (PORTBbits.RB1==1);
+        //restar
+    }
 }
 //******************************************************************************
 // Interrupciones
 //******************************************************************************
 void __interrupt() int1(void){
+    //la configuracion para la interrupcion, en esto realizar antirebote
     if (INTCONbits.T0IF == 1){}
+    //antirebote?
     INTCONbits.T0IF=0;
+    if (PIR1bits.ADIF==1){}
+    PIR1bits.ADIF=0;
     
 }
